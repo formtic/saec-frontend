30b44d34d99f11eb0a640a5ed5d8e31472b2dbda
Darkpool645 <angel_camargo2020@outlook.com>
2025-05-08 12:35:28 -0600

    feat. sidebar integration with admin role

---
+++ src/components/common/Sidebar.vue
@@ -0,0 +1,64 @@
+<template>
+  <aside
+    :class="[ 
+      'bg-[#0D5A79] h-screen sticky top-0 flex flex-col transition-all duration-300 ease-in-out',
+      isCollapsed ? 'w-16' : 'w-60'
+    ]"
+  >
+    <div class="p-4">
+      <img :src="logo" alt="Logo" class="w-2/4" v-if="!isCollapsed" />
+      <img :src="logo" alt="Logo" class="w-8 mx-auto" v-else />
+    </div>
+
+    <div class="flex flex-col flex-grow">
+      <SidebarMenu
+        :options="topMenuOptions"
+        :active-key="activeKey"
+        :collapsed="isCollapsed"
+        @select="onMenuSelect"
+      />
+      <div class="flex-grow" />
+      <SidebarMenu
+        :options="bottomMenuOptions"
+        :active-key="activeKey"
+        :collapsed="isCollapsed"
+        @select="onMenuSelect"
+      />
+    </div>
+
+    <button
+      @click="isCollapsed = !isCollapsed"
+      class="absolute top-1/2 -right-5 transform -translate-y-1/2 z-50 cursor-pointer size-10 flex items-center justify-center bg-white text-gray-700 rounded-full shadow-md hover:bg-gray-100 transition"
+    >
+      <ArrowCircleRightFilled :class="isCollapsed ? 'rotate-180' : ''" class="transition-transform text-[#0D5A79] size-10" />
+    </button>
+  </aside>
+</template>
+
+<script setup>
+import SidebarMenu from '@/components/common/SidebarMenu.vue';
+import logo from '@/assets/svg/logoDarkmode.svg';
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { ArrowCircleRightFilled } from '@vicons/material';
+
+defineProps({
+  topMenuOptions: Array,
+  bottomMenuOptions: Array,
+  onMenuSelect: Function
+});
+
+const route = useRoute();
+const isCollapsed = ref(false);
+const activeKey = ref('');
+
+watch(() => route.path, (newPath) => {
+  activeKey.value = getActiveKeyFromPath(newPath);
+}, { immediate: true });
+
+function getActiveKeyFromPath(path) {
+  const parts = path.split('/').filter(p => p);
+  const base = parts[1] || 'dashboard';
+  return base;
+}
+</script>
---
+++ src/components/common/SidebarMenu.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="text-white">
+    <li
+      v-for="item in options"
+      :key="item.key"
+      @click="$emit('select', item.key)"
+      class="relative group"
+    >
+      <div
+        :class="[
+          'flex items-center py-3 px-4 cursor-pointer hover:bg-[#187DAC] transition-all',
+          activeKey === item.key ? 'bg-[#187DAC]' : '',
+        ]"
+      >
+        <component :is="item.icon" class="size-5" />
+        <span
+          class="ml-3 transition-all duration-300"
+          :class="
+            collapsed
+              ? 'opacity-0 scale-0 w-0'
+              : 'opacity-100 scale-100 w-auto whitespace-nowrap'
+          "
+        >
+          {{ item.label }}
+        </span>
+      </div>
+      <span
+        v-if="collapsed"
+        class="absolute left-full top-1/2 -translate-y-1/2 ml-2 bg-gray-800 text-white text-sm px-2 py-1 rounded-xl shadow opacity-0 group-hover:opacity-100 transition-opacity z-50 pointer-events-none whitespace-nowrap"
+      >
+        {{ item.label }}
+      </span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    options: Array,
+    activeKey: String,
+    collapsed: Boolean,
+  },
+};
+</script>
